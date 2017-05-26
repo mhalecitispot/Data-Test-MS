85304cf609838b8970b389867ff7d8a0d89ee4c9
Greg Lawrence <glawrence001@gmail.com>
2017-05-25 21:08:50 -0500

    Initial
    How to read a JSON string

---
+++ DataTest-MS/DataTest_MS/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataTest_MS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            GetData();
+
+        }
+        private void GetData()
+        {
+
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString("https://facebook.github.io/react-native/movies.json");
+
+                var J2 = JsonConvert.DeserializeObject<RootObject>(json);
+
+                L1.Text = J2.title;
+                L2.Text = J2.description;
+                MyListView.ItemsSource = J2.movies.ToList();
+            }
+        }
+
+        
+    }
+}
---
+++ DataTest-MS/DataTest_MS/Movies.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataTest_MS
+{
+
+    public class Movie
+    {
+        public string title { get; set; }
+        public string releaseYear { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public List<Movie> movies { get; set; }
+    }
+}
